c0ebb768b3dd39c4ad6453e0a2aa2c1239c350b4
Priyatham Sai Chand <priyathamsaichand@gmail.com>
2021-08-24 23:39:28 +0530

    add custom files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,29 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "aki_site",
+    siteTitle: `Akkkiiii site`,
+    siteTitleAlt: `Cara - @lekoarts/gatsby-theme-cara`,
+    siteHeadline: ``,
+    siteUrl: `localhost:8080`,
+    siteDescription: ``,
+    siteLanguage: `en`,
+    siteImage: `/banner.jpg`,
+    author: `Priyatham <priyathamsaichand@gmail.com>`,
   },
-  plugins: [],
-};
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `sections`,
+        path: `${__dirname}/src/sections`,
+      },
+    },
+   {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        lessBabel: true,
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-theme-ui`,
+  ],
+}
